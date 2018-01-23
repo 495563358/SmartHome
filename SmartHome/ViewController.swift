9783094b07c33c6a7ac0ae430aef4d012bb803f5
Smart house <smarthouse@smartdemac-mini.local>
2018-01-23 09:01:52 +0800

    Initial Commit

---
+++ SmartHome/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartHome
+//
+//  Created by Smart house on 2018/1/23.
+//  Copyright © 2018年 Verb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
